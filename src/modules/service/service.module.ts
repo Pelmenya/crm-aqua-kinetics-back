bac001d841377ed5d0dd77908070c56c8affe069
Dmitry Lyapin <lyapindm@yandex.ru>
2025-03-12 17:02:57 +0300

    Add AccountCalendar module

--- src/modules/service/service.module.ts
+++ src/modules/service/service.module.ts
@@ -3,11 +3,13 @@ import { ServiceController } from './service.controller';
 import { AccountServiceModule } from './modules/account-service/account-service.module';
 import { ServiceService } from './service.service';
 import { AuthModule } from '../auth/auth.module';
+import { AccountCalendarModule } from './modules/account-calendar/account-calendar.module';
 
 @Module({
     imports: [
         AuthModule,
         AccountServiceModule,
+        AccountCalendarModule,
     ],
     controllers: [ServiceController],
     providers: [ServiceService]
