c8fc594113db03bfb360af5b97f12141c229e1a4
Dmitry Lyapin <lyapindm@yandex.ru>
2025-03-13 09:16:13 +0300

    Add CreateCalendarWorkDayDto

---
+++ src/modules/service/modules/calendar-work-day/types/create-calendar-work-day.dto.ts
@@ -0,0 +1,31 @@
+import { IsDateString, IsInt, Min, Max } from 'class-validator';
+
+export class CreateCalendarWorkDayDto {
+    @IsDateString()
+    date: Date;
+
+    @IsInt()
+    @Min(0)
+    @Max(6)
+    dayOfWeek: number;
+
+    @IsInt()
+    @Min(0)
+    @Max(23)
+    startHour: number;
+
+    @IsInt()
+    @Min(0)
+    @Max(59)
+    startMinute: number;
+
+    @IsInt()
+    @Min(0)
+    @Max(23)
+    endHour: number;
+
+    @IsInt()
+    @Min(0)
+    @Max(59)
+    endMinute: number;
+}
